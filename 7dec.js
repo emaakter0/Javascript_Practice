c81438a0b793f64f58057ce92539d45e5dbf05af
EMA AkTER <joynobhossainema@gmail.com>
2023-12-07 23:15:02 +0600

    Area shapes

---
+++ 7dec.js
@@ -0,0 +1,10 @@
+var base = parseFloat(prompt("Enter the base = "));
+var height = parseFloat(prompt("Enter the height = "));
+// Area of Rectangle
+var area = base * height;  
+document.write("Area of Rectangle = " + area + "</br>");  
+
+// Area of triangle
+var area = base * height/2;  
+document.write("Area of triangle  = " + area); 
+
